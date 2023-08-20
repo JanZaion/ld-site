675716285b93dcbfa79196ab530c86a70b9c816a
JanZaion <zajic815@gmail.com>
2023-08-20 14:11:53 +0200

    Revert "refactor: organize registry.tsx"
    This reverts commit 2226f8ab564eddae6b6371f69a3eb2e33d374bf7.

--- lib/registry.tsx
+++ lib/registry.tsx
@@ -1,11 +1,14 @@
 'use client';
 
 import { useServerInsertedHTML } from 'next/navigation';
-import { useState } from 'react';
-import type { ReactNode } from 'react';
+import React, { useState } from 'react';
 import { ServerStyleSheet, StyleSheetManager } from 'styled-components';
 
-const StyledComponentsRegistry = ({ children }: { children: ReactNode }) => {
+export default function StyledComponentsRegistry({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   // Only create stylesheet once with lazy initial state
   // x-ref: https://reactjs.org/docs/hooks-reference.html#lazy-initial-state
   const [styledComponentsStyleSheet] = useState(() => new ServerStyleSheet());
@@ -13,16 +16,14 @@ const StyledComponentsRegistry = ({ children }: { children: ReactNode }) => {
   useServerInsertedHTML(() => {
     const styles = styledComponentsStyleSheet.getStyleElement();
     styledComponentsStyleSheet.instance.clearTag();
-    return styles;
+    return <>{styles}</>;
   });
 
-  if (typeof window !== 'undefined') return children;
+  if (typeof window !== 'undefined') return <>{children}</>;
 
   return (
     <StyleSheetManager sheet={styledComponentsStyleSheet.instance}>
       {children}
     </StyleSheetManager>
   );
-};
-
-export default StyledComponentsRegistry;
+}
