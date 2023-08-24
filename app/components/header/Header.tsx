59139fadb6ed3cdbf32d14f407737552cbcf40fe
JanZaion <zajic815@gmail.com>
2023-08-24 23:02:04 +0200

    feat: style header

--- app/components/header/Header.tsx
+++ app/components/header/Header.tsx
@@ -6,24 +6,24 @@ export const Header = () => {
   return (
     <header className={styles.header}>
       <nav>
-        <ul>
+        <ul className={styles.nav}>
           <li>
-            <Link href="#home">Home</Link>
+            <Link href="/#home">Home</Link>
           </li>
           <li>
-            <Link href="#about">About</Link>
+            <Link href="/#about">About</Link>
           </li>
           <li>
-            <Link href="#services">Services</Link>
+            <Link href="/#services">Services</Link>
           </li>
           <li>
-            <Link href="#work">Work</Link>
+            <Link href="/#work">Work</Link>
           </li>
           <li>
-            <Link href="#blog">Blog</Link>
+            <Link href="/posts">Blog</Link>
           </li>
           <li>
-            <Link href="#contact">Contact</Link>
+            <Link href="/#contact">Contact</Link>
           </li>
         </ul>
       </nav>
