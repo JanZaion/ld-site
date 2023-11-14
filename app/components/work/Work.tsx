41d23e4d59e103b3c15f2548260d6db65f255d0e
JanZaion <zajic815@gmail.com>
2023-11-14 13:47:25 +0100

    feat: make works bullet points

--- app/components/work/Work.tsx
+++ app/components/work/Work.tsx
@@ -1,63 +1,65 @@
 export const Work = () => (
   <section id="work">
     <h2>Ukázky mé práce</h2>
-    <p>
-      Postavení organického kanálu který v současnosti generuj ⅔ všech MQLs pro
-      SaaS vendora z oboru zabezpečení přístupu k firemním datům, aplikacím a
-      sítím.
-    </p>
-    <p>
-      Vytvoření firemního a produktového massagingu pro společnost Flowmon
-      Networks (nynější Progress), která působí na poli zabezpečení sítí, včetně
-      media relations a copywritingu.
-    </p>
-    <p>
-      Redesign obsahu a struktury webových stránek pro českého vendora z oblasti
-      log managementu tak, aby reflektoval expanzi firmy do zahraničí.
-    </p>
-    <p>
-      Publikace článků, rozhovorů, tiskových zpráv a komentářů ve
-      vysokoprofilových IT a channel médiích, jako{' '}
-      <a
-        href="https://www.techradar.com/news/this-company-is-giving-away-a-free-vpn-to-businesses-and-theres-no-catches"
-        rel="noreferrer"
-        target="_blank"
-      >
-        TechRadar
-      </a>
-      {', '}
-      <a
-        href="https://securityboulevard.com/2020/01/encrypted-traffic-analysis-will-be-mandatory-soon/"
-        rel="noreferrer"
-        target="_blank"
-      >
-        Security Boulevard
-      </a>
-      {', '}
-      <a
-        href="https://www.darkreading.com/attacks-breaches/ransomware-alarming-growth-verizon-dbir"
-        rel="noreferrer"
-        target="_blank"
-      >
-        Dark reading
-      </a>
-      {', '}
-      <a
-        href="https://www.lupa.cz/clanky/svetove-ojedineli-cesky-goodaccess-spousti-vpn-v-cloudu-zdarma-bez-reklamy-a-sberu-dat/"
-        rel="noreferrer"
-        target="_blank"
-      >
-        Lupa
-      </a>
-      {', '}
-      <a
-        href="https://hn.cz/c1-66542810-flowmon-networks-zvysil-v-lonskem-roce-trzby-pocet-zamestnancu-i-zakazniku"
-        rel="noreferrer"
-        target="_blank"
-      >
-        Hospodářské noviny
-      </a>
-      .
-    </p>
+    <ul>
+      <li>
+        Postavení organického kanálu který v současnosti generuj ⅔ všech MQLs
+        pro SaaS vendora z oboru zabezpečení přístupu k firemním datům,
+        aplikacím a sítím.
+      </li>
+      <li>
+        Vytvoření firemního a produktového massagingu pro společnost Flowmon
+        Networks (nynější Progress), která působí na poli zabezpečení sítí,
+        včetně media relations a copywritingu.
+      </li>
+      <li>
+        Redesign obsahu a struktury webových stránek pro českého vendora z
+        oblasti log managementu tak, aby reflektoval expanzi firmy do zahraničí.
+      </li>
+      <li>
+        Publikace článků, rozhovorů, tiskových zpráv a komentářů ve
+        vysokoprofilových IT a channel médiích, jako{' '}
+        <a
+          href="https://www.techradar.com/news/this-company-is-giving-away-a-free-vpn-to-businesses-and-theres-no-catches"
+          rel="noreferrer"
+          target="_blank"
+        >
+          TechRadar
+        </a>
+        {', '}
+        <a
+          href="https://securityboulevard.com/2020/01/encrypted-traffic-analysis-will-be-mandatory-soon/"
+          rel="noreferrer"
+          target="_blank"
+        >
+          Security Boulevard
+        </a>
+        {', '}
+        <a
+          href="https://www.darkreading.com/attacks-breaches/ransomware-alarming-growth-verizon-dbir"
+          rel="noreferrer"
+          target="_blank"
+        >
+          Dark reading
+        </a>
+        {', '}
+        <a
+          href="https://www.lupa.cz/clanky/svetove-ojedineli-cesky-goodaccess-spousti-vpn-v-cloudu-zdarma-bez-reklamy-a-sberu-dat/"
+          rel="noreferrer"
+          target="_blank"
+        >
+          Lupa
+        </a>
+        {', '}
+        <a
+          href="https://hn.cz/c1-66542810-flowmon-networks-zvysil-v-lonskem-roce-trzby-pocet-zamestnancu-i-zakazniku"
+          rel="noreferrer"
+          target="_blank"
+        >
+          Hospodářské noviny
+        </a>
+        .
+      </li>
+    </ul>
   </section>
 );
